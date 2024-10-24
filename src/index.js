207c9323963232b5435ff372101ecd457e3ac00e
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-10-24 15:47:55 -0300

    Probando interacciones

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { usuarios } from "./modules/usuario.js";
+import { crearUsuario } from "./lib/factory.js";
+
+const admin = crearUsuario("admin", {
+  nombre: "gonzalo",
+  email: "gonzalo@email.com",
+  clave: "123",
+});
+
+console.log(usuarios);
+
+const editor = crearUsuario("editor", {
+  nombre: "agustin",
+  email: "agustin@email.com",
+  clave: "123",
+});
+
+const regular = crearUsuario("regular", {
+  nombre: "emanuel",
+  email: "emanuel@email.com",
+  clave: "123",
+});
+
+admin.agregarUsuario(editor);
+admin.agregarUsuario(regular);
+
+console.log(usuarios);
