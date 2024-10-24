942ed5f227dd10232cd5c90124227523dc27d3f5
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-10-24 16:32:19 -0300

    Uso de esString

--- src/modules/contenido.js
+++ src/modules/contenido.js
@@ -1,21 +1,30 @@
 import { obtenerUID } from "../lib/generator.js";
+import { esString } from "../utils/validator.js";
 
-class Contenido {
+export class Contenido {
   constructor(titulo, texto) {
-    this.id = obtenerUID();
-    this.titulo = titulo;
-    this.texto = texto;
+    let id = obtenerUID();
+    let titulo = titulo;
+    let texto = texto;
   }
 
-  editarTitulo(titulo) {
-    this.titulo = titulo;
+  set titulo(nuevoTitulo) {
+    if (!esString(nuevoTitulo)) throw new Error("Título no válido.");
+
+    this.titulo = nuevoTitulo;
   }
 
-  editarTexto(texto) {
-    this.texto = texto;
+  get titulo() {
+    return this.titulo;
+  }
+
+  set texto(nuevoTexto) {
+    if (!esString(nuevoTexto)) throw new Error("Texto no válido.");
+
+    this.texto = nuevoTexto;
+  }
+
+  get texto() {
+    return this.texto;
   }
 }
-
-module.exports = {
-  Contenido,
-};
