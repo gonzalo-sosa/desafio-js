d8a8910f53b3f39b2d1ea2f3c118b29548e9d80b
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-10-24 16:21:57 -0300

    Watch para desarrollo

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+const { watch } = require("fs");
 const path = require("path");
 
 module.exports = {
   mode: "development",
+  watch: true,
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
