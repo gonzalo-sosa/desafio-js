25da6c5cefe62d98d642d91497d8ca90b5a28a8a
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-10-25 12:49:36 -0300

    Try catch para imprimir errores

--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
 import { usuarios } from "./modules/usuario.js";
 import { crearUsuario } from "./lib/factory.js";
 
-const admin = crearUsuario("admin", {
-  nombre: "gonzalo",
-  email: "gonzalo@email.com",
-  clave: "123",
-});
+try {
+  const admin = crearUsuario("admin", {
+    nombre: "gonzalo",
+    email: "gonzalo@email.com",
+    clave: "123",
+  });
 
-console.log({ usuarios });
+  console.log("Los usuarios existentes son:", usuarios);
 
-const editor = crearUsuario("editor", {
-  nombre: "agustin",
-  email: "agustin@email.com",
-  clave: "123",
-});
+  const editor = crearUsuario("editor", {
+    nombre: "agustin",
+    email: "agustinemail.com",
+    clave: "123",
+  });
 
-const regular = crearUsuario("regular", {
-  nombre: "emanuel",
-  email: "emanuel@email.com",
-  clave: "123",
-});
+  const regular = crearUsuario("regular", {
+    nombre: "emanuel",
+    email: "emanuel@email.com",
+    clave: "123",
+  });
 
-admin.agregarUsuario(editor);
-admin.agregarUsuario(regular);
-admin.eliminarUsuario(regular);
+  admin.agregarUsuario(editor);
+  admin.agregarUsuario(regular);
 
-console.log({ usuarios });
+  admin.eliminarUsuario(); // ERROR
 
-for (let usuario of usuarios) {
-  usuario.login();
-  usuario.logout();
+  console.log(usuarios);
+
+  for (let usuario of usuarios) {
+    usuario.login();
+    usuario.logout();
+  }
+} catch (error) {
+  console.log(error.message);
 }
