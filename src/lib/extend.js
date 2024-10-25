527c1feb7a1c7cad60c7d635fc2a0e0aa1ee479b
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-10-25 12:21:30 -0300

    JSDoc para explicación de función extend

--- src/lib/extend.js
+++ src/lib/extend.js
@@ -1,3 +1,8 @@
+/**
+ * Establece herencia de prototipos de Parent a Child.
+ * @param {Function} Child - La función constructor de la clase child.
+ * @param {Function} Parent - La función constructor de la clase parent.
+ */
 export function extend(Child, Parent) {
   Child.prototype = Object.create(Parent.prototype);
   Child.prototype.constructor = Child;
