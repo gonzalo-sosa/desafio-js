33151a834c73b9e5ea63659ee74e10f395f1aa8b
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-10-25 14:06:59 -0300

    Agregado getter para email

--- src/modules/usuario.js
+++ src/modules/usuario.js
@@ -1,6 +1,6 @@
 // TODO: verificación de email
 // TODO: hash de clave
-import { obtenerUID, cifrarClave } from "../lib/generator.js";
+import { obtenerUUID, cifrarClave } from "../lib/generator.js";
 import { verificarEmail } from "../utils/validator.js";
 
 export var usuarios = []; // Variable global
@@ -13,11 +13,11 @@ const _sesionIniciada = new WeakMap();
 export function Usuario(nombre, email, clave) {
   verificarEmail(email);
 
-  _id.set(this, obtenerUID());
+  _id.set(this, obtenerUUID().then((uuid) => uuid));
   _nombre.set(this, nombre.trim());
   _email.set(this, email.trim());
   _sesionIniciada.set(this, false);
-  let _clave = cifrarClave(clave);
+  let _clave = cifrarClave(clave).then((claveCifrada) => claveCifrada);
 
   Object.defineProperty(this, "id", {
     get: function () {
@@ -28,6 +28,12 @@ export function Usuario(nombre, email, clave) {
   Object.defineProperty(this, "nombre", {
     get: function () {
       return _nombre.get(this);
+    },
+  });
+
+  Object.defineProperty(this, "email", {
+    get: function () {
+      return _email.get(this);
     },
   });
 }
