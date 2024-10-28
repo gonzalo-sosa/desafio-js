22545afdfb5106ff3f348b4a4640b21080cc065e
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-10-28 11:07:55 -0300

    Modificación para resaltar que es el bundle

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    filename: "[name].bundle.js",
     clean: true,
   },
   optimization: {
