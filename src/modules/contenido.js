59685c82c10c9a72c40f74c3268f37f2c8d112d7
gonzalo <gonzalo.sosa@moxit.com.ar>
2024-10-28 11:21:14 -0300

    Modificado nombres de propiedades para evitar recursividad

--- src/modules/contenido.js
+++ src/modules/contenido.js
@@ -4,28 +4,28 @@ import { esString } from "../utils/validator.js";
 export class Contenido {
   constructor(titulo, texto) {
     let _id = obtenerUUID();
-    this.titulo = titulo;
-    this.texto = texto;
+    this._titulo = titulo;
+    this._texto = texto;
   }
 
   set titulo(nuevoTitulo) {
     if (!esString(nuevoTitulo)) throw new Error("Título no válido.");
 
-    this.titulo = nuevoTitulo;
+    this._titulo = nuevoTitulo;
   }
 
   get titulo() {
-    return this.titulo;
+    return this._titulo;
   }
 
   set texto(nuevoTexto) {
     if (!esString(nuevoTexto)) throw new Error("Texto no válido.");
 
-    this.texto = nuevoTexto;
+    this._texto = nuevoTexto;
   }
 
   get texto() {
-    return this.texto;
+    return this._texto;
   }
 }
 
